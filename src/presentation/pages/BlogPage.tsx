1434e9dba2c26f80e189d266546b3f2f9f82d5e6
Lâm Tấn Phú <phultse182607@fpt.edu.vn>
2026-06-02 10:36:39 +0700

    [Hotfix]Quick fix for blogs

--- src/presentation/pages/BlogPage.tsx
+++ src/presentation/pages/BlogPage.tsx
@@ -4,7 +4,7 @@ import { VSCodeLayout }   from '../templates/VSCodeLayout'
 import { BlogCard }       from '../molecules/BlogCard'
 import { BlogPreview }    from '../molecules/BlogPreview'
 import { ActiveFilterTab } from '../molecules/ActiveFilterTab'
-import { BlogSidebar } from '../organisms/BlogSideBar'
+import { BlogSidebar } from '../organisms/BlogSidebar'
 
 // =============================================================================
 // BlogPage — Page
